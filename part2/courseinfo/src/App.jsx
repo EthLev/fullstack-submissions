e1b348ad7048e5fe7cbb0381d59a7ed5a1f12036
Ethan Levitt <28455177@sun.ac.za>
2026-09-20 18:03:13 +0200

    ex 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,28 +1,59 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+
+  return (
+    <div>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  )
 }
+  
 
 
 const Course = ({ course }) => {
@@ -35,20 +66,10 @@ const Course = ({ course }) => {
   )
 }
 
-/*const Total = ({ parts }) => {
-  let totalTally = 0
-  parts.forEach(part => {
-    totalTally += part.exercises
-    console.log(totalTally)
-  })
-  return (
-    <b>total of {totalTally} exercises</b>
-  )
-}*/
-
 
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => {
+    console.log('What is happening', sum, part)
   return sum + part.exercises
   }, 0)
 
