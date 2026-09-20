b8341ad4c2be393c31c0970aa9f782b70936d8f8
Ethan Levitt <28455177@sun.ac.za>
2026-09-20 17:11:57 +0200

    ex 2.1 completed

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -24,4 +24,53 @@ const App = () => {
   return <Course course={course} />
 }
 
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Part = ( {part} ) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Header = ({ name }) => {
+  return(
+    <h1>{name}</h1>
+  )
+}
+
+/*
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+
+      const Note = ({ note }) => {
+  return <li>{note.content}</li>
+}
+*/
+
+
 export default App
